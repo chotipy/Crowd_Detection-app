c1b5ef0204b4397b3cccb8f9b376ea074ce7828f
balqismuharda <balqis.muharda@gmail.com>
2025-12-15 17:38:35 +0700

    Fix layout

--- app.py
+++ app.py
@@ -435,7 +435,6 @@ st.markdown(
         text-transform: uppercase;
         letter-spacing: 0.05em;
         color: {text_secondary};
-        margin-bottom: 1rem;
     }}
 
     section[data-testid="stSidebar"] label {{
