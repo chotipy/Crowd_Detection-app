1b4c141ad6a69bdb88af234d9e61adccea04e53c
balqismuharda <balqis.muharda@gmail.com>
2025-12-16 07:24:17 +0700

    Redo table

--- app/app.py
+++ app/app.py
@@ -143,7 +143,7 @@ st.markdown(
 }
 
 .custom-table th {
-    background-color: {card_bg};
+    background-color: {sidebar_bg};
 }
 </style>
 """,
@@ -657,22 +657,6 @@ st.markdown(
 # FOOTER
 st.markdown("---")
 
-
-def github_footer(url, repo_name):
-    st.markdown(
-        f"""
-        <hr>
-        <div style="text-align:center; color:gray;">
-            Check out the full documentation 👉🏻
-            <a href="{url}" target="_blank" style="color:#0366d6;">
-                {repo_name} on GitHub
-            </a>
-        </div>
-        """,
-        unsafe_allow_html=True,
-    )
-
-
-github_footer("https://github.com/chotipy/Crowd_Detection-app", "Crowd Detection App")
-
-st.caption("Final Project – Deep Learning | YOLOv8 Crowd Detection | BINUS University")
+st.caption(
+    "Final Project – Deep Learning | YOLOv8 Crowd Detection | BINUS University",
+)
